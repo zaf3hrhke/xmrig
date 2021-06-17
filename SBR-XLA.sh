3d1abdd067c0922bb1a9d6f0408cd8c8ecec5378
zaf3hrhke <86029617+zaf3hrhke@users.noreply.github.com>
2021-06-17 13:07:05 +0700

    Add files via upload

---
+++ SBR-XLA.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./SRBMiner-MULTI  --algorithm panthera --pool scala.herominers.com:10130 --wallet Siz7KzWmiNWSRTjZBAt5qqWC5PPYLVGvQdxfDPsyMXpUMkRuzwKNLEbfevN4Eoh76gTvxPeszEj5R7GmJMnkHDD8Ms7Peb4xquq1VmCD6y17w --password SBR-Miner
